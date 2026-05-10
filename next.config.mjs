571f73d709ee2005bbef2df076465ab70dc598db
Rajesh <pbrajesh2004@gmail.com>
2026-05-10 21:01:04 +0530

    fix: trigger rebuild

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
   reactStrictMode: true,
   allowedDevOrigins: ['127.0.0.1'],
   output: 'export',
+  basePath: isProd ? '/veducate-ecatalogue' : '',
+  assetPrefix: isProd ? '/veducate-ecatalogue/' : '',
   images: {
     unoptimized: true,
   },
